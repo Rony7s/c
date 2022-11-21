bf51fb4d4842526c30021984ddae003276c68281
Rony Ahmmed <30334499+Rony7s@users.noreply.github.com>
2022-11-21 20:49:23 +0600

    Create AllArea.c
    All area of ....

---
+++ lab-1/AllArea.c
@@ -0,0 +1,28 @@
+/*  Name:Md. Rony Ahmmed Shah [2002035]
+    Name:
+    Name:
+    Group:
+*/
+#include <stdio.h>
+#define PI 3.14159
+int main() {
+
+
+    float A, B, C, tringle_area, circle_area, trapezium_area, square_b, rectangle;
+    printf("Enter the value of A B C\n");
+    scanf("%f %f %f", &A, &B, &C);
+
+    tringle_area = 0.5*A*C;
+    circle_area = PI *C*C;
+    trapezium_area = 0.5*(A+B)*C;
+    square_b = B*B;
+    rectangle = A*B;
+
+    printf("TRIANGULO: %.3f\n", tringle_area);
+    printf("CIRCULO: %.3f\n", circle_area);
+    printf("TRAPEZIO: %.3f\n", trapezium_area);
+    printf("QUADRADO: %.3f\n", square_b);
+    printf("RETANGULO: %.3f\n", rectangle);
+
+    return 0;
+}
