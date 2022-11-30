061f0281cdfc5281a1d9fc86dd9324735e980097
Rony Ahmmed <30334499+Rony7s@users.noreply.github.com>
2022-12-01 00:15:06 +0600

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = -2;
+    char c = 'e';
+    
+    switch(c){
+        case 'a':
+        printf("Welcome to class One");
+        break;
+        
+        case 'b':
+        printf("Welcome to class Two");
+        break;
+        
+        case 'c':
+        printf("Welcome to class Three");
+        break;
+        
+        default :
+        printf("Welcome to BDU");
+    
+    }
+
+    return 0;
+}
