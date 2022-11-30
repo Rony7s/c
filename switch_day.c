89b935aa1c26c2f39053224b0750281c6f759b55
Rony Ahmmed <30334499+Rony7s@users.noreply.github.com>
2022-12-01 00:26:08 +0600

    Create switch_day.c
    a simple example of switch with day

---
+++ switch_day.c
@@ -0,0 +1,41 @@
+// this code use switch and days .... .....
+// Rony Ahmmed BDU-ICTE
+#include<stdio.h>
+int main(){
+    
+    int day;
+    
+    printf("Enter a number 1 to 7\n");
+    scanf("%d", &day);
+    
+    switch (day) {
+      
+      case 1:
+        printf("Sunday");
+        break;
+      case 2:
+        printf("Monday");
+        break;
+      case 3:
+        printf("Tuesday");
+        break;
+      case 4:
+        printf("Wednesday");
+        break;
+      case 5:
+        printf("Thursday");
+        break;
+      case 6:
+        printf("Friday");
+        break;
+      case 7:
+        printf("Saturday");
+        break;
+      default :
+        printf(" :( ");
+        break;
+        
+    }
+
+    return 0;
+}
